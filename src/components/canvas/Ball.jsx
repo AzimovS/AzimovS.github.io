02c398e87833edbd0cdc67319822df9b8d212f51
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-07-01 16:13:46 +0300

    fix(): typo

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -7,12 +7,14 @@ import {
   Preload,
   useTexture,
 } from '@react-three/drei';
+
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <Float speed={1.75} rotationIntensity={1}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -26,8 +28,9 @@ const Ball = (props) => {
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          flatShading
+          scale={1}
           map={decal}
+          flatShading
         />
       </mesh>
     </Float>
@@ -36,10 +39,14 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball igmUrl={icon} />
+        <Ball imgUrl={icon} />
       </Suspense>
 
       <Preload all />
