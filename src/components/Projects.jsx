e8185ad89a5433d9581d4c55919c696f545dbce0
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-07-03 23:10:09 +0300

    feat(): add contacts

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const ProjectCard = ({
               <img
                 src={github}
                 alt='github'
-                className='w-1/ h-1/2 object-contain'
+                className='w-1/2 h-1/2 object-contain'
               />
             </div>
           </div>
