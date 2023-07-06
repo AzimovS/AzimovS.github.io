7d4688e52c126b6d7c682991d45507d0474e3f79
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-07-06 21:29:11 +0300

    fix(): deploy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/azimovs.github.io/',
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
   }
+
+  if (command !== 'serve') {
+    config.base = '/'
+  }
+
+  return config
 })
