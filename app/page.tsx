47d362f95dbac440f365b76b4978982265384ff5
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-09-24 10:08:43 +0200

    feat: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import IntroSection from "./components/IntroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <IntroSection />
         {/* <AchievementsSection />
